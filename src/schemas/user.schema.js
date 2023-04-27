30a884bc9e1e9675559214725f790b1588a2f506
winiciuskyutoku <winicius123098@gmail.com>
2023-04-27 11:46:35 -0300

    fix: Mensagem de erro na senha em pt-br

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -3,7 +3,9 @@ import joi from "joi";
 export const signupSchema = joi.object({
     name: joi.string().required(),
     email: joi.string().email().required(),
-    password: joi.string().required().min(3)
+    password: joi.string().required().min(3).message({
+        "string.min": "A senha deve ter no minimo 3 caracteres"
+    })
 })
 
 export const signinSchema = joi.object({
