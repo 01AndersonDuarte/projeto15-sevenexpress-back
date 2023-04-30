0aa6cd859d9fb065951a68d27a78c4066b47ce37
winiciuskyutoku <winicius123098@gmail.com>
2023-04-30 10:02:19 -0300

    feat: Delete do produto Carrinho

--- src/routes/carrinho.routes.js
+++ src/routes/carrinho.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { authValidation } from "../middlewares/authValidation.middleware.js";
-import { getCarrinho, postCarrinho } from "../controllers/carrinho.controller.js";
+import { deleteCarrinho, getCarrinho, postCarrinho } from "../controllers/carrinho.controller.js";
 
 const carrinhoRouter = Router()
 
 //carrinhoRouter.use(authValidation)
 carrinhoRouter.post("/carrinho", postCarrinho)
 carrinhoRouter.get("/carrinho/:id", getCarrinho)
+carrinhoRouter.post("/carrinho/:id", deleteCarrinho)
 
 export default carrinhoRouter
